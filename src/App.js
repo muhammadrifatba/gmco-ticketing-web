0b8224cfba1f5dc21f49edec634671153d62bd61
Daffa Romero <daffaromero@mail.ugm.ac.id>
2022-09-28 22:56:51 +0700

    Membenarkan form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/HomePage/Home';
 import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom'
-import FormOne from './components/FormOne';
-import FormBelajar from './components/FormBelajar';
 import Form from './components/Form'
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/FormOne' element={<FormOne/>} />
-          <Route path='/FormBelajar' element={<FormBelajar/>} />
           <Route path='/form' element={<Form/>} />
         </Routes>
     </Router>
