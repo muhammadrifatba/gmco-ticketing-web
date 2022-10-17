f9c5ba1cb4ecad61c8a37e7efa885c7c57f428f5
Daffa Romero <daffaromero@mail.ugm.ac.id>
2022-10-17 22:50:21 +0700

    membenarkan useNavigate

--- src/components/pages/SeatInvoice.js
+++ src/components/pages/SeatInvoice.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect , useNavigate} from 'react'
+import React, { useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import '../style/SeatInvoice.css'
 
